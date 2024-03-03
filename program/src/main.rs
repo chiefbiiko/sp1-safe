8e1efb9c4cda4e2c5b901a6ce3000cb1e5edc2eb
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-03 11:15:45 +0100

    wip: program flow

--- program/src/main.rs
+++ program/src/main.rs
@@ -1,4 +1,4 @@
-//! A simple program to be proven inside the zkVM.
+//! Prove a storage value in zk (non-doxing) in the zkVM.
 
 #![no_main]
 sp1_zkvm::entrypoint!(main);
@@ -11,13 +11,6 @@ fn keccak256_2(a: &[u8], b: &[u8], out: &mut [u8; 32]) {
     keccak256.update(b);
     keccak256.finalize(&mut out);
 }
-//HASHING
-// let mut keccak256 = Keccak::v256();
-// let mut output = [0u8; 32];
-// keccak256.update(b"hello");
-// keccak256.update(b" ");
-// keccak256.update(b"world");
-// keccak256.finalize(&mut output);
 
 pub fn main() {
 
@@ -25,5 +18,20 @@ pub fn main() {
     // let rolling_root...
 
 
+    // read inputs
+    // blockhash      0:32
+    //   TODO add all components to recalc blockhash
+    // address        32:52
+    //   nonce        52:84
+    //   balance      84:116
+    //   storageRoot  116:148
+    //   codeHash     148:180
+    
+    // storageKey     180:212
+    // storageValue   212:244
+    // siblings       244...
+                
+
+    // prove storage root recalcable given storageKey and siblings
 
 }
