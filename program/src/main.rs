cc91f6e419f180909beeb37d1b27f40f0c90b8b7
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-03 19:02:36 +0100

    wip: more noop

--- program/src/main.rs
+++ program/src/main.rs
@@ -30,6 +30,17 @@ fn keccak256_2(a: &[u8], b: &[u8], out: &mut [u8; 32]) {
 //         value: The storage value.
 //         proof: An array of RLP-serialized MerkleTree-Nodes, starting with the storageHash-Node, following the path of the SHA3 (key) as path.
 
+// https://github.com/polytope-labs/solidity-merkle-trees/blob/f637bb083eb6bdef3daee016e300a56800c2725e/src/MerklePatricia.sol#L133
+// /**
+//  * @notice Verifies ethereum specific merkle patricia proofs as described by EIP-1188.
+//     * @param root hash of the merkle patricia trie
+//     * @param proof a list of proof nodes
+//     * @param keys a list of keys to verify
+//       @param expectedValue 
+//     * @return bytes[] a list of values corresponding to the supplied keys.
+//     */
+// function VerifyEthereumProof(bytes32 root, bytes[] memory proof, bytes[] memory keys)
+
 pub fn main() {
 
     // read inputs
