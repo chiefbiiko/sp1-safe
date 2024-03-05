a0b4d34c3d578b9f8c386f8c3fcad033d3ebd86d
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-05 20:34:23 +0100

    docs: modocs

--- program/src/main.rs
+++ program/src/main.rs
@@ -1,4 +1,15 @@
-//! Prove contract storage in zk (non-doxing) in the zkVM.
+//! Proves a Safe multisig in zero-knowledge in the sp1 zkVM.
+//! Allows proving that a message has been "signed", by a Safe via the 
+//! SignMessage library without doxing the Safe and/or its owners. Still 
+//! "signed" hereby means approved by at least $threshold owners of the Safe.
+//! The circuit takes a message hash, corresponding EIP1186 storage proof, and 
+//! state root as inputs and has two outputs: the proven state root and a hash 
+//! of the storage and account keys. The output state root must be used to 
+//! calculate the corresponding block hash which in turn must be checked for 
+//! authenticity on-chain using the blockhash opcode given the block number at 
+//! which the storage proof was generated. The latter output hash serves as a 
+//! blinded linking anchor that allows associating given proof to a particular 
+//! Safe by recomputing the hash given the account and storage keys.
 
 #![no_main]
 sp1_zkvm::entrypoint!(main);
@@ -26,33 +37,23 @@ fn bytes64(a: [u8; 32], b: [u8; 32]) -> [u8; 64] {
 }
 
 pub fn main() {
-    // read inputs
     let inputs = sp1_zkvm::io::read::<Inputs>();
     let _state_root = H256(inputs.state_root);
     let _storage_root = H256(inputs.storage_root);
 
-    // verify storage proof
+    // Verify storage proof
     let db = StorageProof::new(inputs.storage_proof).into_memory_db::<KeccakHasher>();
     let trie = TrieDBBuilder::<EIP1186Layout<KeccakHasher>>::new(&db, &_storage_root).build();
     let val = trie.get(&inputs.storage_key).expect("storage trie read failed").expect("target storage node is none");
+    // Safe's SignMessageLib marks messages as "signed" with a literal 1
     assert_eq!(val, vec![1u8], "msg not signed");
 
-    // verify account proof
+    // Verify account proof
     let db = StorageProof::new(inputs.account_proof).into_memory_db::<KeccakHasher>();
     let trie = TrieDBBuilder::<EIP1186Layout<KeccakHasher>>::new(&db, &_state_root).build();
     let ok = trie.contains(&inputs.account_key).expect("account check failed");
     assert!(ok, "account proof failed");
 
-    // This proven state root must be used to calculate the corresponding 
-    // block hash given all other block header components. That resulting block 
-    // hash must be checked for authenticity on-chain using the blockhash opcode
-    // given the block number at which the storage proof was generated
     sp1_zkvm::io::write_slice(&inputs.state_root);
-    // To link this proof to a particular Safe recompute below hash
-    // (probably in your dapp's primary circuit) given the account and storage 
-    // keys. The account key is a postimage of the Safe address serving as 
-    // linking anchor whereas the storage key being a postimage of the message
-    // hash serves as the linking blinding.
-    // keccak256(storage_key + account_key)
     sp1_zkvm::io::write_slice(&keccak_256(&bytes64(inputs.storage_key, inputs.account_key)))
 }
