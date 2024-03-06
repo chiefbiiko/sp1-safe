cf52863f9e0cde4fa60fe3e01448d146c9111c68
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-06 12:27:06 +0100

    refactor: first draft complete

--- script/src/main.rs
+++ script/src/main.rs
@@ -1,18 +1,31 @@
 //! A simple script to generate and verify the proof of a given program.
 
 use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_safe_basics::Inputs;
+use const_hex;
 mod util;
-// use util::fetch_inputs;
+use util::fetch_inputs;
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
-fn main() {
-    // Generate proof.
-    let mut stdin = SP1Stdin::new();
-    // let n = 186u32;
-    // stdin.write(&n);
-    //TODO construct and write input
+fn bytes20(x: Vec<u8>) -> [u8; 20] {
+    x.try_into().expect("invalid address")
+}
 
+fn bytes32(x: Vec<u8>) -> [u8; 32] {
+    x.try_into().expect("invalid hash")
+}
+
+#[tokio::main]
+async fn main() {
+    // Assemble and write inputs
+    let mut stdin = SP1Stdin::new();
+    let safe = bytes20(const_hex::decode(std::env::var("SAFE").expect("must set env var SAFE=0x...")).expect("not hex"));
+    let msg_hash = bytes32(const_hex::decode(std::env::var("MSG_HASH").expect("must set env var MSG_HASH=0x...")).expect("not hex"));
+    let inputs = fetch_inputs(safe.into(), msg_hash.into()).await;
+    stdin.write::<Inputs>(&inputs);
+
+    // Generate proof
     // NOTE only executing instead of proving/verifying while dev
     // let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
     // let a = proof.stdout.read::<u128>();
@@ -24,7 +37,7 @@ fn main() {
     println!("state root: {:02X?}", state_root);
     println!("blind safe: {:02X?}", blind_safe);
 
-    // // Verify and save proof.
+    // // Verify and save proof
     // SP1Verifier::verify(ELF, &proof).expect("verification failed");
     // proof
     //     .save("proof-with-io.json")
--- script/src/util.rs
+++ script/src/util.rs
@@ -1,4 +1,3 @@
-use anyhow::Result;
 use ethers::{
     providers::{Middleware, Provider},
     types::{Address, H256}, utils::keccak256,
@@ -6,15 +5,15 @@ use ethers::{
 use sp1_safe_basics::{bytes64, Inputs, SAFE_SIGNED_MESSAGES_SLOT};
 use zerocopy::AsBytes;
 
-pub async fn fetch_inputs(safe: Address, msg_hash: H256) -> Result<Inputs> {
-    let provider = Provider::try_from("https://rpc.gnosis.gateway.fm")?;
+pub async fn fetch_inputs(safe: Address, msg_hash: H256) -> Inputs {
+    let provider = Provider::try_from("https://rpc.gnosis.gateway.fm").expect("rpc provider failed");
 
     let account_key = keccak256(&safe);
     let storage_key = keccak256(bytes64(msg_hash.into(), SAFE_SIGNED_MESSAGES_SLOT));
 
-    let latest = provider.get_block_number().await?;
-    let header = provider.get_block(latest).await?.expect("no such block");
-    let proof = provider.get_proof(safe, vec![storage_key.into()], Some(latest.into())).await?;
+    let latest = provider.get_block_number().await.expect("fetching latest failed");
+    let header = provider.get_block(latest).await.expect("fetching block failed").expect("no such block");
+    let proof = provider.get_proof(safe, vec![storage_key.into()], Some(latest.into())).await.expect("fetching proof failed");
 
     let inputs = Inputs {
         msg_hash: msg_hash.into(),
@@ -26,5 +25,5 @@ pub async fn fetch_inputs(safe: Address, msg_hash: H256) -> Result<Inputs> {
         storage_proof: proof.storage_proof[0].proof.iter().map(|b| b.as_bytes().to_vec()).collect(),
     };
 
-    Ok(inputs)
+    inputs
 }
