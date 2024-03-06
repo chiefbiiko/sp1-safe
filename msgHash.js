1a0a1da645ad699aabcf1b1855b47271ebd602a1
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-06 20:25:56 +0100

    workaround: safe sign msg lib cli helpers

---
+++ msgHash.js
@@ -0,0 +1,25 @@
+const { Contract, JsonRpcProvider } = require("ethers")
+
+if (!process.env.SAFE || !process.env.MSG) {
+  throw Error("must set env var SAFE=0x... and MSG=0x...")
+}
+
+async function main() {
+  const safe = new Contract(
+    process.env.SAFE,
+    [
+      "function getMessageHash(bytes memory message) public view returns (bytes32)"
+    ],
+    {
+      provider: new JsonRpcProvider(
+        process.env.RPC ?? "https://rpc.gnosis.gateway.fm"
+      )
+    }
+  )
+
+  const msgHash = await safe.getMessageHash(process.env.MSG)
+
+  console.log("msgHash", msgHash)
+}
+
+main()
---
+++ proposeSignMsg.js
@@ -0,0 +1,61 @@
+const { default: Safe, Web3Adapter } = require("@safe-global/protocol-kit")
+const SafeApiKit = require("@safe-global/api-kit")
+const ethers = require("ethers")
+
+const SIGN_MSG_LIB = "0xd53cd0aB83D845Ac265BE939c57F53AD838012c9"
+
+if (!process.env.SAFE || !process.env.MSG || !process.env.PRIVATE_KEY) {
+  throw Error("must set env var SAFE=0x... PRIVATE_KEY=0x... and MSG=abcd...")
+}
+
+async function main() {
+  const provider = new ethers.JsonRpcProvider(process.env.RPC)
+
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+
+  const ethAdapter = new EthersAdapter({
+    ethers,
+    signerOrProvider: signer
+  })
+
+  const safeSigner = await Safe.create({
+    ethAdapter,
+    safeAddress: process.env.SAFE
+  })
+
+  const rawData = config.registry.interface.encodeFunctionData("signMessage", [
+    Buffer.from(process.env.MSG, "utf8")
+  ])
+  const safeTransactionData = {
+    to: SIGN_MSG_LIB,
+    data: rawData,
+    operation: 1, // delegateCall
+    value: "0"
+  }
+  const safeTx = await safeSigner.createTransaction({ safeTransactionData })
+
+  const apiKit = new SafeApiKit({
+    chainId: 100
+  })
+
+  // Deterministic hash based on transaction parameters
+  const safeTxHash = await safeSigner.getTransactionHash(safeTx)
+
+  // Sign transaction to verify that the transaction is coming from owner 1
+  const senderSignature = await safeSigner.signHash(safeTxHash)
+
+  await apiKit.proposeTransaction({
+    safeAddress,
+    safeTransactionData: safeTransaction.data,
+    safeTxHash,
+    senderAddress: await owner1Signer.getAddress(),
+    senderSignature: senderSignature.data
+  })
+
+  console.log(
+    `proposed: Safe ---delegatecall---> SignMessageLib.signMessage("${process.env.MSG}")`
+  )
+  console.log("safe tx hash", safeTxHash)
+}
+
+main()
