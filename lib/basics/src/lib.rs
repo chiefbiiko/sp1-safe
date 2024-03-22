b48cb522865bcb96a13000761c7f9a6b27b9d249
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-22 09:00:52 +0100

    wip: blockhash recomputation

--- lib/basics/src/lib.rs
+++ lib/basics/src/lib.rs
@@ -1,10 +1,12 @@
 use ethers::{
+    utils,
     providers::{Middleware, Provider},
-    types::{Address, H256},
+    types::{Address, H256, Block},
 };
 use serde::{Deserialize, Serialize};
 use tiny_keccak::{Hasher, Keccak};
 use zerocopy::AsBytes;
+use rlp::RlpStream;
 
 pub const SAFE_SIGNED_MESSAGES_SLOT: [u8; 32] = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
@@ -17,10 +19,10 @@ pub struct Inputs {
     pub state_root: [u8; 32],   // eth_getBlockBy*::response.stateRoot
     pub storage_root: [u8; 32], // eth_getProof::response.storageHash
     pub account_key: [u8; 32],  // keccak256(address)
-    // slot of the signedMessages mapping within Safe storage equals 5
     pub storage_key: [u8; 32],       // keccak256(msg_hash + uint256(5))
     pub account_proof: Vec<Vec<u8>>, // eth_getProof::response.accountProof
     pub storage_proof: Vec<Vec<u8>>, // eth_getProof::response.storageProof.proof
+    pub header_rlp: Vec<u8>,         // RLP-encoded header
 }
 
 pub fn coerce_bytes20(x: Vec<u8>) -> [u8; 20] {
@@ -59,7 +61,7 @@ pub async fn fetch_inputs(rpc: &str, safe: Address, msg_hash: H256) -> Inputs {
         .get_block_number()
         .await
         .expect("fetching latest failed");
-    let header = provider
+    let block = provider
         .get_block(latest)
         .await
         .expect("fetching block failed")
@@ -69,10 +71,16 @@ pub async fn fetch_inputs(rpc: &str, safe: Address, msg_hash: H256) -> Inputs {
         .await
         .expect("fetching proof failed");
 
+    println!("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    println!("block.hash {:?}", &block.hash.unwrap());
+    let header_rlp =  rlp_encode(&block);
+    println!("computed blockhash {:?}", H256(keccak256(&header_rlp)));
+    println!("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+
     Inputs {
         safe: safe.into(),
         msg_hash: msg_hash.into(),
-        state_root: header.state_root.into(),
+        state_root: block.state_root.into(),
         storage_root: proof.storage_hash.into(),
         account_key,
         storage_key,
@@ -86,5 +94,56 @@ pub async fn fetch_inputs(rpc: &str, safe: Address, msg_hash: H256) -> Inputs {
             .iter()
             .map(|b| b.as_bytes().to_vec())
             .collect(),
+        header_rlp,//WIP
     }
+}
+
+// https://ethereum.stackexchange.com/a/67332
+// https://github.com/ethereum/go-ethereum/blob/14eb8967be7acc54c5dc9a416151ac45c01251b6/core/types/block.go#L65
+// pub hash: Option<H256>,
+// block hash should be recomputable with the following header components:
+// pub parent_hash: H256,
+// pub uncles_hash: H256,
+// pub author: Option<H160>,
+// pub state_root: H256,
+// pub transactions_root: H256,
+// pub receipts_root: H256,
+// pub logs_bloom: Option<Bloom>,
+// pub difficulty: U256,
+// pub number: Option<U64>,
+// pub gas_limit: U256,
+// pub gas_used: U256,
+// pub timestamp: U256,
+// pub extra_data: Bytes,
+// pub mix_hash: Option<H256>,
+// pub nonce: Option<H64>,
+// pub base_fee_per_gas: Option<U256>,
+// pub withdrawals_root: Option<H256>,
+// pub blob_gas_used: Option<U256>,
+// pub excess_blob_gas: Option<U256>,
+// pub parent_beacon_block_root: Option<H256>,
+pub fn rlp_encode(block: &Block<H256>) -> Vec<u8> {
+    let mut rlp = RlpStream::new();
+    rlp.begin_list(20);
+    rlp.append(&block.parent_hash);
+    rlp.append(&block.uncles_hash);
+    rlp.append(&block.author);
+    rlp.append(&block.state_root);
+    rlp.append(&block.transactions_root);
+    rlp.append(&block.receipts_root);
+    rlp.append(&block.logs_bloom);
+    rlp.append(&block.difficulty);
+    rlp.append(&block.number);
+    rlp.append(&block.gas_limit);
+    rlp.append(&block.gas_used);
+    rlp.append(&block.timestamp);
+    rlp.append(&block.extra_data.as_bytes().to_vec());
+    rlp.append(&block.mix_hash);
+    rlp.append(&block.nonce);
+    rlp.append(&block.base_fee_per_gas);         // london
+    rlp.append(&block.withdrawals_root);         // shanghai
+    rlp.append(&block.blob_gas_used);            // cancun
+    rlp.append(&block.excess_blob_gas);          // cancun
+    rlp.append(&block.parent_beacon_block_root); // cancun
+    rlp.out().freeze().into()
 }
