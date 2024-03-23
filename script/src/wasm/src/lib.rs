bf804fafb58d6ce9463f3ce0ed9b61052fcafafd
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-23 10:58:58 +0100

    wip: wasm_bindgen setup

---
+++ script/src/wasm/src/lib.rs
@@ -0,0 +1,64 @@
+use sp1_core::{SP1Prover, SP1Stdin};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_test::wasm_bindgen_test;
+
+const ELF: &[u8] = include_bytes!("../../../../program/elf/riscv32im-succinct-zkvm-elf");
+
+#[wasm_bindgen]
+pub struct Wrapper {
+    blockhash: Vec<u8>,
+    challenge: Vec<u8>,
+    proof: Vec<u8>,
+}
+
+#[wasm_bindgen]
+impl Wrapper {
+    #[wasm_bindgen(constructor)]
+    pub fn new(blockhash: Vec<u8>, challenge: Vec<u8>, proof: Vec<u8>) -> Wrapper {
+        Wrapper {
+            blockhash,
+            challenge,
+            proof,
+        }
+    }
+    pub fn blockhash(&self) -> Vec<u8> {
+        self.blockhash.clone()
+    }
+    pub fn challenge(&self) -> Vec<u8> {
+        self.challenge.clone()
+    }
+    pub fn proof(&self) -> Vec<u8> {
+        self.proof.clone()
+    }
+}
+
+#[wasm_bindgen]
+pub fn prove(inputs: &[u8]) -> Wrapper {
+    let mut stdin = SP1Stdin::new();
+    stdin.write_slice(&inputs);
+
+    let mut proofwio = SP1Prover::prove(ELF, stdin).expect("proving failed");
+    // let mut stdout = SP1Prover::execute(ELF, stdin).expect("execution failed");
+
+    let blockhash = proofwio.stdout.read::<[u8; 32]>();
+    let challenge = proofwio.stdout.read::<[u8; 32]>();
+
+    let wrapper = Wrapper::new(
+        blockhash.to_vec(),
+        challenge.to_vec(),
+        bincode::serialize(&proofwio.proof).expect("serialization failed"),
+    );
+
+    return wrapper;
+}
+
+#[wasm_bindgen_test]
+pub fn test_prove() {
+    let witness = b"";
+
+    let wrapper = prove(witness);
+
+    assert_eq!(wrapper.blockhash().len(), 32);
+    assert_eq!(wrapper.challenge().len(), 32);
+    assert!(wrapper.proof().len() > 0);
+}
