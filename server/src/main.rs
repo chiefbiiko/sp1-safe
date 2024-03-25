2152852b23737f587b802d29cc8f9594db55ecc5
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-25 10:53:34 +0100

    wip: dbg server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,90 +1,77 @@
-use actix_web::{http::StatusCode, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer, ResponseError};
-use anyhow::{anyhow, Result};
-// use serde::{Deserialize, Serialize};
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::json::{json, Json, Value};
 use sp1_core::{SP1Prover, SP1Stdin};
-use sp1_safe_basics::{coerce_bytes20, coerce_bytes32, Inputs, Sp1SafeParams, Sp1SafeResult};
+use sp1_safe_basics::{Inputs, Sp1SafeParams, Sp1SafeResult};
 use sp1_safe_fetch::fetch_inputs;
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
-
 const SEPOLIA_RPC: &str = "https://rpc.sepolia.dev";
 const GNOSIS_RPC: &str = "https://rpc.gnosis.gateway.fm";
 
-async fn prove(body: web::Bytes) -> Result<HttpResponse> {
-    let params = serde_json::from_slice::<Sp1SafeParams>(&body)?;
+#[post("/", data = "<params>")]
+async fn index(params: Json<Sp1SafeParams>) -> Value {
     let rpc = match params.chain_id {
         100 => GNOSIS_RPC,
         11155111 => SEPOLIA_RPC,
-        _ => "",
+        _ => panic!("invalid chain_id"),
     };
-    let safe: [u8; 20] = const_hex::decode_to_array::<&str, 20>(&params.safe_address)?;
-    let msg_hash: [u8; 32] = const_hex::decode_to_array::<&str, 32>(&params.message_hash)?;
+    let safe: [u8; 20] =
+        const_hex::decode_to_array::<&str, 20>(&params.safe_address).expect("safe");
+    let msg_hash: [u8; 32] =
+        const_hex::decode_to_array::<&str, 32>(&params.message_hash).expect("msg_hash");
 
-    let (anchor, inputs) = fetch_inputs(&rpc, safe.into(), msg_hash.into()).await?;
+    let (anchor, inputs) = fetch_inputs(&rpc, safe.into(), msg_hash.into())
+        .await
+        .expect("fetch");
     let mut stdin = SP1Stdin::new();
     stdin.write::<Inputs>(&inputs);
 
-    let mut proofwio = SP1Prover::prove(ELF, stdin)?;
+    let mut proofwio = SP1Prover::prove(ELF, stdin).expect("prove");
 
     let blockhash = proofwio.stdout.read::<[u8; 32]>();
     let challenge = proofwio.stdout.read::<[u8; 32]>();
 
-    let res = Sp1SafeResult {
-        safe_address: params.safe_address,
-        message_hash: params.message_hash,
+    json!(Sp1SafeResult {
+        safe_address: params.safe_address.to_owned(),
+        message_hash: params.message_hash.to_owned(),
         blocknumber: anchor,
         blockhash: format!("0x{}", const_hex::encode(blockhash)),
         challenge: format!("0x{}", const_hex::encode(challenge)),
-        proof: format!("0x{}", const_hex::encode(bincode::serialize(&proofwio.proof)?)),
-    };
-
-    Ok(HttpResponse::Ok().json(res)) // <- send response
+        proof: format!(
+            "0x{}",
+            const_hex::encode(bincode::serialize(&proofwio.proof).expect("bincode"))
+        ),
+    })
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-
-    log::info!("starting HTTP server at http://localhost:8080");
-
-    HttpServer::new(|| {
-        App::new()
-            // enable logger
-            .wrap(middleware::Logger::default())
-            .app_data(web::JsonConfig::default().limit(4096)) // <- limit size of the payload (global configuration)
-            .service(web::resource("/").route(web::post().to(prove)))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
 
 #[cfg(test)]
-mod tests {
-    use actix_web::{body::to_bytes, dev::Service, http, test};
-
+mod test {
     use super::*;
+    use rocket::{http::Status, local::blocking::Client};
 
-    #[actix_web::test]
-    async fn test_index() {
-        let app =
-            test::init_service(App::new().service(web::resource("/").route(web::post().to(prove))))
-                .await;
-
-        let req = test::TestRequest::post()
-            .uri("/")
-            .set_json(Sp1SafeParams {
+    #[test]
+    fn test_index() {
+        println!("hi1");
+        let client = Client::tracked(super::rocket()).expect("valid rocket instance");
+        println!("hi2");
+        let response = client
+            .post(uri!(super::index))
+            .json(&Sp1SafeParams {
                 chain_id: 100,
                 safe_address: "0x38Ba7f4278A1482FA0a7bC8B261a9A673336EDDc".to_owned(),
                 message_hash: "0xa225aed0c0283cef82b24485b8b28fb756fc9ce83d25e5cf799d0c8aa20ce6b7"
                     .to_owned(),
             })
-            .to_request();
-        let resp = app.call(req).await.unwrap();
-
-        assert_eq!(resp.status(), http::StatusCode::OK);
-
-        let body_bytes = to_bytes(resp.into_body()).await.unwrap();
-        assert!(body_bytes.len() > 0);
+            .dispatch();
+        println!("hi3");
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
     }
 }
