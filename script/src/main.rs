e47d906b44ed01439573417b0ed45613abcff937
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-25 17:23:56 +0100

    wip: exclude 2big proof from json output

--- script/src/main.rs
+++ script/src/main.rs
@@ -1,8 +1,11 @@
 //! A simple script to generate the proof of the sp1-safe program.
 
 use const_hex;
+use std::fs::File;
+use std::io::prelude::*;
+use serde_json::json;
 use sp1_core::{SP1Prover, SP1Stdin /*, SP1Verifier*/};
-use sp1_safe_basics::Inputs;
+use sp1_safe_basics::{Inputs, Sp1SafeResult};
 use sp1_safe_fetch::fetch_inputs;
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
@@ -11,6 +14,7 @@ const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf
 async fn main() {
     sp1_core::utils::setup_logger();
     // Assemble and write inputs
+    // let out = std::env::var("OUT_FILE").expect("must set env var OUT_FILE=/tmp/xyz");
     let rpc = std::env::var("RPC").unwrap_or("https://rpc.gnosis.gateway.fm".to_string());
     let safe = const_hex::decode_to_array::<&str, 20>(
         &std::env::var("SAFE").expect("must set env var SAFE=0x..."),
@@ -28,27 +32,39 @@ async fn main() {
 
     // Generate proof
     let mut proofwio = SP1Prover::prove(ELF, stdin).expect("proving failed");
-    // let mut stdout = SP1Prover::execute(ELF, stdin).expect("execution failed");
 
     let blockhash = proofwio.stdout.read::<[u8; 32]>();
     let challenge = proofwio.stdout.read::<[u8; 32]>();
 
+    // let mut file = File::create(&out).expect("out file");
+    // file.write_all(
+    //     json!(Sp1SafeResult {
+    //         safe_address: format!("0x{}", const_hex::encode(safe)),
+    //         message_hash: format!("0x{}", const_hex::encode(msg_hash)),
+    //         blocknumber: anchor,
+    //         blockhash: format!("0x{}", const_hex::encode(blockhash)),
+    //         challenge: format!("0x{}", const_hex::encode(challenge)),
+    //         proof: format!(
+    //             "0x{}",
+    //             const_hex::encode(bincode::serialize(&proofwio.proof).expect("bincode"))
+    //         ),
+    //     }).to_string().as_bytes()
+    // ).expect("write file");
     println!(
-        "Safe multisig storage proof verification passed
-=== proof params ===
-Safe address: 0x{}
-msg hash: 0x{}
-block number: {}
-=== proof outputs ===
-blockhash: 0x{}
-challenge: 0x{}",
-        // proof: 0x{}",
-        const_hex::encode(&safe),
-        const_hex::encode(&msg_hash),
-        anchor,
-        const_hex::encode(&blockhash),
-        const_hex::encode(&challenge),
-        // const_hex::encode(bincode::serialize(&proofwio.proof).expect("bincode")),
+        "{}",
+        json!(Sp1SafeResult {
+            safe_address: format!("0x{}", const_hex::encode(safe)),
+            message_hash: format!("0x{}", const_hex::encode(msg_hash)),
+            blocknumber: anchor,
+            blockhash: format!("0x{}", const_hex::encode(blockhash)),
+            challenge: format!("0x{}", const_hex::encode(challenge)),
+            proof: "0x".to_string()
+            // format!(
+            //     "0x{}",
+            //     const_hex::encode(bincode::serialize(&proofwio.proof).expect("bincode"))
+            // ),
+        })
+        .to_string()
     );
 
     // Verify and save proof
