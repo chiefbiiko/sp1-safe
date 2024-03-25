e5eb20e6d3a1f43b41a43a2f75bc51eb2c2a70b1
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-03-25 10:20:54 +0100

    refactor: replace coerce_* with const_hex::decode_to_array()

--- script/src/main.rs
+++ script/src/main.rs
@@ -2,7 +2,7 @@
 
 use const_hex;
 use sp1_core::{SP1Prover, SP1Stdin /*, SP1Verifier*/};
-use sp1_safe_basics::{coerce_bytes20, coerce_bytes32, Inputs};
+use sp1_safe_basics::Inputs;
 use sp1_safe_fetch::fetch_inputs;
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
@@ -12,14 +12,14 @@ async fn main() {
     sp1_core::utils::setup_logger();
     // Assemble and write inputs
     let rpc = std::env::var("RPC").unwrap_or("https://rpc.gnosis.gateway.fm".to_string());
-    let safe = coerce_bytes20(
-        const_hex::decode(std::env::var("SAFE").expect("must set env var SAFE=0x..."))
-            .expect("not hex"),
-    );
-    let msg_hash = coerce_bytes32(
-        const_hex::decode(std::env::var("MSG_HASH").expect("must set env var MSG_HASH=0x..."))
-            .expect("not hex"),
-    );
+    let safe = const_hex::decode_to_array::<&str, 20>(
+        &std::env::var("SAFE").expect("must set env var SAFE=0x..."),
+    )
+    .expect("env var SAFE");
+    let msg_hash = const_hex::decode_to_array::<&str, 32>(
+        &std::env::var("MSG_HASH").expect("must set env var MSG_HASH=0x..."),
+    )
+    .expect("env var MSG_HASH");
     let (anchor, inputs) = fetch_inputs(&rpc, safe.into(), msg_hash.into())
         .await
         .expect("fetch_inputs failed");
