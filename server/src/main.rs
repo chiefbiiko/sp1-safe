7d46d1a852f55ce736117d0c34c3716ee5a9019a
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-05-19 15:25:46 +0200

    refactor: gen groth16 proofs for verification on the evm

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,7 +12,7 @@ use rocket::{
 };
 use sp1_safe_basics::{Inputs, Sp1SafeParams, Sp1SafeResult};
 use sp1_safe_fetch::fetch_inputs;
-use sp1_sdk::{SP1Prover, SP1Stdin};
+use sp1_sdk::{ProverClient, SP1Stdin};
 use std::env;
 use std::net::Ipv4Addr;
 
@@ -25,18 +25,24 @@ async fn _proof(params: Json<Sp1SafeParams>) -> Result<Value> {
         11155111 => env::var("SEPOLIA_RPC").unwrap_or("https://1rpc.io/sepolia".to_string()),
         _ => bail!("invalid chain_id {}", params.chain_id),
     };
+
     let safe: [u8; 20] = const_hex::decode_to_array::<&str, 20>(&params.safe_address)?;
     let msg_hash: [u8; 32] = const_hex::decode_to_array::<&str, 32>(&params.message_hash)?;
+
     log::info!("🕳️ fetching inputs");
     let (anchor, inputs) = fetch_inputs(&rpc, safe.into(), msg_hash.into()).await?;
     let mut stdin = SP1Stdin::new();
     stdin.write::<Inputs>(&inputs);
+
     log::info!("🎰 zk proving");
-    let mut proofwio = SP1Prover::prove(ELF, stdin)?;
-    log::info!("🎱 serving results");
-    let blockhash = proofwio.stdout.read::<[u8; 32]>();
-    let challenge = proofwio.stdout.read::<[u8; 32]>();
-    // let proofbin = bincode::serialize(&proofwio.proof)?;
+    let client = ProverClient::new();
+    let (pk, _vk) = client.setup(ELF);
+    let mut proofwpv = client.prove_groth16(&pk, stdin).expect("proving failed");
+
+    let blockhash = proofwpv.public_values.read::<[u8; 32]>();
+    let challenge = proofwpv.public_values.read::<[u8; 32]>();
+    let proofbin = bincode::serialize(&proofwpv.proof)?;
+
     Ok(json!(Sp1SafeResult {
         chain_id: params.chain_id,
         safe_address: params.safe_address.to_owned(),
@@ -44,10 +50,7 @@ async fn _proof(params: Json<Sp1SafeParams>) -> Result<Value> {
         blocknumber: anchor,
         blockhash: format!("0x{}", const_hex::encode(blockhash)),
         challenge: format!("0x{}", const_hex::encode(challenge)),
-        proof: format!(
-            "0x{}",
-            "" // const_hex::encode(proofbin)
-        ),
+        proof: format!("0x{}", const_hex::encode(proofbin)),
     }))
 }
 
