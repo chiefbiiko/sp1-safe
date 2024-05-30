7f2dbd3dfe6a84b10941d815696687d889d7f01f
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-05-30 11:17:15 +0200

    feat: plonk verifier

--- verifier/src/main.rs
+++ verifier/src/main.rs
@@ -5,11 +5,11 @@ use std::path::PathBuf;
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
-    let client = ProverClient::new();
-    let (_pk, vk) = client.setup(ELF);
+    // let client = ProverClient::new();
+    // let (_pk, vk) = client.setup(ELF);
 
     sp1_sdk::artifacts::export_solidity_plonk_bn254_verifier(PathBuf::from(env!("CARGO_MANIFEST_DIR")))
         .expect("failed to export verifier");
 
-    println!("✞✞✞✞✞ vk {}", const_hex::encode(&vk.hash_bytes()));
+    // println!("✞✞✞✞✞ vk {}", const_hex::encode(&vk.hash_bytes()));
 }
