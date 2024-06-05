13cc2709b227e3d794852019c2ae686f55802afa
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-06-05 08:26:18 +0200

    fix: SP1CoreOpts import

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,7 +12,7 @@ use rocket::{
     serde::json::{json, Json, Value},
     Config, Response,
 };
-use sp1_core::SP1CoreOpts;
+sp1_core::utils::SP1CoreOpts;
 use sp1_safe_basics::{Inputs, Sp1SafeParams, Sp1SafeResult};
 use sp1_safe_fetch::fetch_inputs;
 use sp1_sdk::{HashableKey, ProverClient, SP1ProvingKey, SP1Stdin, SP1VerifyingKey};
